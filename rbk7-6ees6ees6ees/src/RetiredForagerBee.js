dfbc595d5c84df398fe1a4dfcffa905b125251b8
AdamMomen <adammuman81@gmail.com>
2019-12-04 15:54:52 +0100

    PROGRESS

--- rbk7-6ees6ees6ees/src/RetiredForagerBee.js
+++ rbk7-6ees6ees6ees/src/RetiredForagerBee.js
@@ -1,3 +1,11 @@
-class RetiredForagerBee {
-  // TODO..
-};
+class RetiredForagerBee extends ForagerBee {
+  constructor(age = 40, color = 'grey', food, job = 'gamble', canFly = false) {
+    super(age, color, food, job, canFly);
+  }
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+  gamble() {
+    this.treasureChest.push('wining');
+  }
+}
