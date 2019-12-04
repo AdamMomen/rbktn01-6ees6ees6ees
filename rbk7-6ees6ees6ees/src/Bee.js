b707be622ed67613f87de1f112977ef04e2af5de
AdamMomen <adammuman81@gmail.com>
2019-12-04 15:25:22 +0100

    PROGRESS

--- rbk7-6ees6ees6ees/src/Bee.js
+++ rbk7-6ees6ees6ees/src/Bee.js
@@ -1,6 +1,6 @@
 class Bee extends Grub {
-  constructor(age, color, food) {
-    super(age = 5, color = 'yellow', food);
+  constructor(age = 5, color = 'yellow', food) {
+    super(age, color, food);
     this.job = 'Keep on growing';
   }
 }
--- rbk7-6ees6ees6ees/src/HoneyMakerBee.js
+++ rbk7-6ees6ees6ees/src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
-  // TODO..
-};
+class HoneyMakerBee extends Bee {
+  constructor(age = 10, color, food) {
+    super(age, color, food);
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
+}
